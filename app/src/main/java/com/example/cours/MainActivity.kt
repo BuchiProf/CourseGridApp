5417c51c9e5e002d0af008bb53274c4cf73b46a2
BuchiProf <profbuchi@gmail.comm>
2024-12-05 17:59:53 +0100

    grille fonctionnelle

--- app/src/main/java/com/example/cours/MainActivity.kt
+++ app/src/main/java/com/example/cours/MainActivity.kt
@@ -3,27 +3,48 @@ package com.example.cours
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.cours.data.DataSource
+import com.example.cours.model.Topic
+
 import com.example.cours.ui.theme.CoursTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             CoursTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Cours(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CourseApp()
                 }
             }
         }
@@ -31,17 +52,71 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Cours(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TopicGrid(modifier: Modifier = Modifier){
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(8.dp)
+    ) {
+        items(DataSource.topics) { topic ->
+            TopicCard(topic)
+        }
+    }
+}
+
+@Composable
+fun TopicCard(topic : Topic, modifier: Modifier = Modifier) {
+    Card(modifier = modifier) {
+        Row{
+            Image(
+                painter = painterResource(topic.classImageId),
+                contentDescription = stringResource(topic.name),
+                modifier = Modifier
+                    .size(width = 68.dp, height = 68.dp)
+                    .aspectRatio(1f)
+            )
+            Column {
+                Text(
+                    text = stringResource(topic.name),
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(
+                        start = 16.dp,
+                        top = 16.dp,
+                        end = 16.dp,
+                        bottom = 8.dp
+                    )
+                )
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_grain),
+                        contentDescription = null,
+                        modifier = Modifier.padding(
+                            start = 16.dp,
+                            end = 8.dp)
+                    )
+                    Text(
+                        text = topic.classNumber.toString(),
+                        style = MaterialTheme.typography.labelMedium,
+                        modifier = Modifier
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+
+@Composable
+fun CourseApp() {
+    TopicGrid(modifier = Modifier.fillMaxWidth())
 }
 
 @Preview(showBackground = true)
 @Composable
-fun CoursPreview() {
-    CoursTheme {
-        Cours("Android")
+fun CoursePreview() {
+
+        TopicGrid(modifier = Modifier.fillMaxWidth())
     }
-}
